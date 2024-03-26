f362bf24df4b2410f664a405d7137272afd00fb9
Ederson Torresini <etorresini@ifsc.edu.br>
2024-03-26 11:47:36 +0000

    Testar mapa

---
+++ cliente/js/mapa.js
@@ -0,0 +1,42 @@
+export default class mapa extends Phaser.Scene {
+  constructor () {
+    super('mapa')
+  }
+
+  preload () {
+    this.load.tilemapTiledJSON('mapa', './assets/mapa/mapa.json')
+
+    this.load.image('blocos', './assets/mapa/blocos.png')
+    this.load.image('grama', './assets/mapa/grama.png')
+    this.load.image('itens', './assets/mapa/itens.png')
+    this.load.image('paredes', './assets/mapa/paredes.png')
+    this.load.image('pedras', './assets/mapa/pedras.png')
+    this.load.image('personagem', './assets/mapa/personagem.png')
+    this.load.image('plantas', './assets/mapa/plantas.png')
+    this.load.image('sombras-plantas', './assets/mapa/sombras-plantas.png')
+    this.load.image('sombras', './assets/mapa/sombras.png')
+  }
+
+  create () {
+    this.tilemapMapa = this.make.tilemap({ key: 'mapa' })
+
+    this.tilesetBlocos = this.tilemapMapa.addTilesetImage('blocos')
+    this.tilesetGrama = this.tilemapMapa.addTilesetImage('grama')
+    this.tilesetItens = this.tilemapMapa.addTilesetImage('itens')
+    this.tilesetParedes = this.tilemapMapa.addTilesetImage('paredes')
+    this.tilesetPedras = this.tilemapMapa.addTilesetImage('pedras')
+    this.tilesetPersonagem = this.tilemapMapa.addTilesetImage('personagem')
+    this.tilesetPlantas = this.tilemapMapa.addTilesetImage('plantas')
+    this.tilesetSombrasPlantas = this.tilemapMapa.addTilesetImage('sombras-plantas')
+    this.tilesetSombras = this.tilemapMapa.addTilesetImage('sombras')
+
+    this.layerTerreno = this.tilemapMapa.createLayer('terreno', [this.tilesetGrama])
+    this.layerSombras = this.tilemapMapa.createLayer('sombras', [this.tilesetSombrasPlantas, this.tilesetSombras])
+    this.layerPlantas = this.tilemapMapa.createLayer('plantas', [this.tilesetPlantas])
+    this.layerItens = this.tilemapMapa.createLayer('itens', [this.tilesetItens])
+    this.layerParedes = this.tilemapMapa.createLayer('paredes', [this.tilesetBlocos, this.tilesetParedes])
+  }
+
+  update () {
+  }
+}
